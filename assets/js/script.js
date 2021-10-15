8f5c99e21da62757b613a535c31f384533c29da6
Rafael Cardoso <rrafa203@gmail.com>
2021-10-14 19:37:16 -0700

    add questions

---
+++ assets/js/script.js
@@ -0,0 +1,103 @@
+// Creating questions
+
+var quiz = [{
+  question: "1. Which of these is not a JavaScript Type?",
+  answers: [{
+          correct: true,
+          display: "Function"
+      },
+      {
+          correct: false,
+          display: "String"
+      },
+      {
+          correct: false,
+          display: "Number"
+      },
+      {
+          correct: false,
+          display: "Boolean"
+      }
+  ]
+},
+{
+  question: "2. What is the company who developed JavaScript?",
+  answers: [{
+          correct: false,
+          display: "Apple"
+      },
+      {
+          correct: false,
+          display: "Google"
+      },
+      {
+          correct: false,
+          display: "Microsoft"
+      },
+      {
+          correct: true,
+          display: "Netscape"
+      }
+  ]
+},
+{
+  question: "3. Which of these is a loop structure in JavaScript?",
+  answers: [{
+          correct: false,
+          display: "var"
+      },
+      {
+          correct: true,
+          display: "for"
+      },
+      {
+          correct: false,
+          display: "this"
+      },
+      {
+          correct: false,
+          display: "let"
+      }
+  ]
+},
+{
+  question: "4. What is the result of 1+2+”3” in JavaScript?",
+  answers: [{
+          correct: false,
+          display: "6"
+      },
+      {
+          correct: true,
+          display: "33"
+      },
+      {
+          correct: false,
+          display: "9"
+      },
+      {
+          correct: false,
+          display: "123"
+      }
+  ]
+},
+{
+  question: " _______ is an actions that can be performed on object.",
+  answers: [{
+          correct: false,
+          display: "Array"
+      },
+      {
+          correct: true,
+          display: "Method"
+      },
+      {
+          correct: false,
+          display: "Property"
+      },
+      {
+          correct: false,
+          display: "Function"
+      }
+  ]
+}
+];
